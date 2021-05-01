9e45d33164b9f6c31d5533eaef38ba8668272464
João Rocha <78546581+john0x01@users.noreply.github.com>
2021-04-30 23:33:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('teste')
+import pygame
+
+WIDTH = 800
+HEIGHT = 800
+
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('BomberPy')
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        
